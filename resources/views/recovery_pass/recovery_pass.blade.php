02ccd735b1dfcef1913e9df5730797d57fc367e6
Kevin Delva <kdelva@retailsbs.com>
2021-03-04 11:51:01 -0300

    Revert "fix: common issues"
    This reverts commit 8e0b0cbb84db5523f1dfc985707898f525632aa9.

--- resources/views/recovery_pass/recovery_pass.blade.php
+++ resources/views/recovery_pass/recovery_pass.blade.php
@@ -38,7 +38,7 @@ Restablecer Contraseña
     $("#btnrecov").click(function(){
         var pass = $("#pass").val();
         var passConf = $("#passConfirm").val();
-        var id = {{session('id')}};
+        var id = {{Session('id')}}
         if(pass == passConf){
             $.ajax({
                 type: "GET",
