001c728f0a1a16b1421d7dd57b7c91d891707d49
Kevin Delva <kdelva@retailsbs.com>
2021-03-29 19:09:39 -0300

    TEST: Rollback

--- resources/views/home_proxy.blade.php
+++ resources/views/home_proxy.blade.php
@@ -24,6 +24,128 @@ Saint Charles Formularios
 @endsection
 
 @section("context")
-    
+    @if(Session::has('apoderado'))
+        <?php 
+            $var = session::get('apoderado');
+            $id_apo= session::get('apoderado')['id'];   
+        ?>
+        <hr>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    
+                </div>
+                <div class="col-md-4">
+                    <h2 style="text-align: center">Bienvenido <br> {{$var["names"]}} {{$var["last_p"]}} {{$var["last_m"]}}</h2>
+                </div>
+                <div class="col-md-4" style="text-align: right">
+                    <a href="logout" class="btn btn-outline-danger text-danger">Salir</a>
+                </div>
+            </div>
+            <hr>
+            <!--NEW-->
+            @php
+                $home = "";
+                $prof = "";
+                $matr = "";
+                $actPass = "";
+                $homeC = "";
+                $profC = "";
+                $matrC = "";
+                $actPassC = "";
+                $circle_data = "";
+                $circle_dataC = "";
+                if (isset($_GET["active"])) {
+                    $active = $_GET["active"];
+                    if($active == "home"){$home = "active"; $homeC = "show active";}
+                    elseif ($active == "info") {$prof = "active"; $profC = "show active";}
+                    elseif ($active == "circle") {$circle_data = "active"; $circle_dataC = "show active";}
+                    elseif ($active == "matricula") {$matr = "active"; $matrC = "show active";}
+                    else {
+                        $home = "active";
+                        $prof = "";
+                        $matr = "";
+                        $circle_data = "";
+                        $homeC = "show active";
+                        $circle_dataC = "";
+                        $profC = "";
+                        $matrC = "";
+                    }
+                }else{
+                    $home = "active";
+                    $prof = "";
+                    $matr = "";
+                    $circle_data = "";
+                    $homeC = "show active";
+                    $circle_dataC = "";
+                    $profC = "";
+                    $matrC = "";
+                }
+            @endphp
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link {{$home}}" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Resumen</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " id="profile-tab" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">Actualizar mi información</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link {{$circle_data}}" id="circle-tab" data-toggle="tab" href="#circle" role="tab" aria-controls="circle" aria-selected="false">Mi círculo Familiar</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="matriculas-tab" data-toggle="tab" href="#matriculas" role="tab" aria-controls="matriculas" aria-selected="false">Mis Alumnos matriculados 2021</a>
+                    <script>
+                        @if($dataProxy == NULL)
+                            $("#matriculas-tab").remove();
+                        @endif 
+                    </script>
+                </li>
+            </ul>
+            <hr>
+            <div class="tab-content" id="myTabContent">
+                @if($dataProxy == NULL)
+                    <div class="alert alert-danger" role="alert">
+                        Debe completar formulario <a href="/apoderados/home?active=info">Actualizar mi información</a> y <a href="/apoderados/home?active=circle">Mi círculo Familiar</a> para proceder a Ingresar datos de Alumno(s) Año Escolar 2021 <i class="fas fa-exclamation-triangle"></i> 
+                    </div>
+                @endif
+                <div class="tab-pane fade {{$homeC}}" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-body">
+                                    <!--
+                                    <p><strong>Nombre:</strong> {{$var["names"]}} {{$var["last_p"]}} {{$var["last_m"]}}</p>
+                                    <p><strong>Teléfono Celular:</strong> {{$var["cell_phone"]}}</p>
+                                    <p><strong>Email:</strong> {{$var["email"]}}</p>
+                                    -->
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <h3>Comunicaciones {{getenv("MATRICULAS_PARA")}}</h3>
+                            <hr>
+                            <iframe src="https://saintcharlescollege.cl/ins/iframe_news" title="Últimas Noticias" style="width: 100%;height: 95vh;" frameborder="0"></iframe>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-pane fade {{$profC}}" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                    @include('home_proxy_frames.proxy_data')
+                </div>
+                <div class="tab-pane fade {{$circle_dataC}}" id="circle" role="tabpanel" aria-labelledby="circle-tab">
+                    @include('home_proxy_frames.fam_circle')
+                </div>
+                <div class="tab-pane fade {{$matrC}}" id="matriculas" role="tabpanel" aria-labelledby="matriculas-tab">
+                    @include('home_proxy_frames.matriculas')
+                </div>
+            </div>
+            <!--END-->
+        </div>
+    @else
+        <script>
+            $( document ).ready(function() {
+                window.location = "/";
+            });
+        </script>
+    @endif
     
 @endsection
